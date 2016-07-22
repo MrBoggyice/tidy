8bb42a1419ab035eabf50ee61d9126dbfd18d81c
fortune <fortune_ik@yahoo.com>
2016-07-22 21:43:38 +0100

    added a home.html

--- project/home/views.py
+++ project/home/views.py
@@ -17,8 +17,14 @@ home_blueprint = Blueprint(
 
 
 @home_blueprint.route('/', methods=['GET', 'POST'])
+@home_blueprint.route('/index', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
+
+
+@home_blueprint.route('/home')
+def home():
+    return render_template('home.html')
 
 
 # @home_blueprint.route('/confirmation')
